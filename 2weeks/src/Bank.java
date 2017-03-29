de6cd77b5473b3a6baa9dafcba9e2e472ad8ff03
khanj <khanj@DESKTOP-O5U4H5T>
2017-03-29 20:35:11 +0900

    4weeks-abstract account

--- 2weeks/src/Bank.java
+++ 2weeks/src/Bank.java
@@ -22,27 +22,33 @@ public class Bank {
 		
 		System.out.printf("account1 balance : %.2f\n",account1.getBalance());
 		System.out.printf("account1 현재 출금가능액 : %.2f\n",account1.getWithdrawableAccount());
+		
 		System.out.println("account1 went Bankrupt!");
 		
 		System.out.printf("account2 balance : %.2f\n",account2.getBalance());
 		System.out.printf("account2 현재 출금가능액 : %.2f\n",account2.getWithdrawableAccount());
-		System.out.println("6 Month later!");
+		
+		
 		account2.passTime(6);
+		System.out.println("6 Month later!");
+
 		
 		System.out.printf("account2 balance : %.2f\n",account2.getBalance());
 		System.out.printf("account2 현재 출금가능액 : %.2f\n",account2.getWithdrawableAccount());
+		account2.passTime(6);
 		System.out.println("6 Month later!");
 		account2.debit(50);
-		account2.passTime(6);
+
 		
 		System.out.printf("account2 balance : %.2f\n",account2.getBalance());
 		System.out.printf("account2 현재 출금가능액 : %.2f\n",account2.getWithdrawableAccount());
-		
 		
 		account2.passTime(1);
 		System.out.println("next 1 Month later!");
+		
 		account2.debit(50);
 		System.out.println("50달러 인출!!");
+		
 		System.out.printf("account2 balance : %.2f\n",account2.getBalance());
 		System.out.printf("account2 현재 출금가능액 : %.2f\n",account2.getWithdrawableAccount());
 		
